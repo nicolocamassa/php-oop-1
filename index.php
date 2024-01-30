2cf0ee48eeadede7340944325827bdf5431d931a
nicolocamassa <camassanicolo03@gmail.com>
2024-01-30 14:12:57 +0100

    [UPDATE] construct added and print results

--- index.php
+++ index.php
@@ -4,5 +4,19 @@
         public $year;
         public $genre;
         public $adults;
+
+        function __construct($_name, $_year, $_genre){
+            $this->name = $_name;
+            $this->year = $_year;
+            $this->genre = $_genre;
+        }
     }
+
+    $film_1 = new Movie('DemoA', 2000, 'DemoB');
+    $film_1->adults = 'Sì';
+
+    echo $film_1->name.' ';
+    echo $film_1->year.' ';
+    echo $film_1->genre.' ';
+    echo $film_1->adults;
 ?>
